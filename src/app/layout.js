d93c1ef384ce16d4ea0aa52afa898075a278267b
sandunrmst <sandunalugc@gmail.com>
2024-06-15 09:33:29 +0530

    Delete old files

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Suspense } from "react";
-import Loading from "./loading";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Suspense fallback={<Loading/>}>{children}</Suspense>
+        <div>{children}</div>
       </body>
     </html>
   );
