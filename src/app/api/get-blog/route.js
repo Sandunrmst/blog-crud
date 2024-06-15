905da8c452ebcf95de710f14ef6d270108e2e39a
sandunrmst <sandunalugc@gmail.com>
2024-06-15 20:00:16 +0530

    build API rout for get all blogs data from db

--- src/app/api/get-blog/route.js
+++ src/app/api/get-blog/route.js
@@ -1,7 +1,26 @@
+import connectToDB from "@/database";
+import Blog from "@/models/blog";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
+    //connect to tb
+    await connectToDB();
+    //Get all Blog.find({})
+    const extractAllBlogsFromDB = await Blog.find({});
+
+    //if true send message success and fail send false message
+    if (extractAllBlogsFromDB) {
+      return NextResponse({
+        success: true,
+        data: extractAllBlogsFromDB,
+      });
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "Something went wrong! please try again later",
+      });
+    }
   } catch (error) {
     console.log(error);
     return NextResponse.json({
