28cc30ec92b13a53efbccee0ec490ae7a6891eff
sandunrmst <sandunalugc@gmail.com>
2024-06-15 20:18:01 +0530

    get all blogs from db

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,7 +1,31 @@
 import BlogView from "@/components/blog-view";
 import React from "react";
 
-const Blogs = () => {
+async function fetchListOfBlogs() {
+  try {
+    const apiResponse = await fetch("http://localhost:3000/api/get-blogs", {
+      method: "GET",
+      cache: "no-store",
+    });
+
+    const result = await apiResponse.json();
+    return result?.data;
+
+    //This below code in the get-blog api routs in above result?.data check according to api routes
+    // if (extractAllBlogsFromDB) {
+    //   return NextResponse({
+    //     success: true,
+    //     data: extractAllBlogsFromDB,
+    //   });
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+const Blogs = async () => {
+  const blogList = await fetchListOfBlogs();
+  console.log(blogList, "bloglist");
+
   return <BlogView />;
 };
 
