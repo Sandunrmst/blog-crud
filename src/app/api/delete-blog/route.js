37f033b1cfb4348a599d64a594f4abea7f49db1c
sandunrmst <sandunalugc@gmail.com>
2024-06-16 11:03:05 +0530

    build api for  delete a  blog from the db

--- src/app/api/delete-blog/route.js
+++ src/app/api/delete-blog/route.js
@@ -1,7 +1,35 @@
+import connectToDB from "@/database";
+import Blog from "@/models/blog";
 import { NextResponse } from "next/server";
 
 export async function DELETE(req) {
   try {
+    //connect to the DB
+    await connectToDB();
+    const { searchParams } = new URL(req.url);
+    //this will get id from the url
+    const getCurrentBlogID = searchParams.get("id");
+
+    if (!getCurrentBlogID) {
+      return NextResponse.json({
+        success: false,
+        message: "Blog ID is required",
+      });
+    }
+
+    //this will automatically delete from the DB
+    const deleteCurrectBlogID = await Blog.findByIdAndDelete(getCurrentBlogID);
+    if (deleteCurrectBlogID) {
+      return NextResponse.json({
+        success: true,
+        message: "Blog is deleted successfully",
+      });
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "Something went wrong! Please try again.",
+      });
+    }
   } catch (error) {
     console.log(error);
     return NextResponse.json({
